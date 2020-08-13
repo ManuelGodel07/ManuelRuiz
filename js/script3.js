d761ddfb2dd73612b1a9cea874bc4c49e29ef50f
Manuel_Ruiz <manuel_ruiz@ciencias.unam.mx>
2020-08-13 14:50:12 -0500

    completando nomina12

--- js/script3.js
+++ js/script3.js
@@ -1,5 +1,10 @@
-let c = prompt('Dame un numero');
+function limpiar(){
+	document.getElementById("formulario").reset();
+}
+
+/**let c = prompt('Dame un numero');
 const multiplicar = (c,b) => c * b
 
 let wuakamole = (multiplicar (c,5));
 alert('tu numero multiplicado por 5 es: ' + wuakamole);
+**/
