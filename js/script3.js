cb7cdec26dd23b9f36a55bdf08ff591a49595cf2
Manuel_Ruiz <manuel_ruiz@ciencias.unam.mx>
2020-08-13 14:32:04 -0500

    completando nomina10

--- js/script3.js
+++ js/script3.js
@@ -1,15 +1,5 @@
-const  greet = (person, sexo) => {
-	return sexo === 'm' 
-		? `Bienvenido ${person}`
-		: `Bienvenida ${person}`
-}
-
-prompt(greet('manuel','m' ))
-prompt(greet('maria', 'f'))
-/**
 let c = prompt('Dame un numero');
 const multiplicar = (c,b) => c * b
 
 let wuakamole = (multiplicar (c,5));
 alert('tu numero multiplicado por 5 es: ' + wuakamole);
- */
