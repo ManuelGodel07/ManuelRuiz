4e319e33f656a906389110fc409e2af92a1b06d2
Manuel_Ruiz <manuel_ruiz@ciencias.unam.mx>
2020-08-13 21:56:26 -0500

    actualizacion de log

--- js/script2.js
+++ js/script2.js
@@ -1,5 +1,5 @@
 const fila = document.querySelector('.contenedor-carousel');
-const peliculas = document.querySelectorAll('.pelicula');
+const pics = document.querySelectorAll('.pic');
 
 const flechaIzquierda = document.getElementById('flecha-izquierda');
 const flechaDerecha = document.getElementById('flecha-derecha');
@@ -29,33 +29,18 @@ flechaIzquierda.addEventListener('click', () => {
 
 
 // ? ----- ----- Hover ----- -----
-peliculas.forEach((pelicula) => {
-	pelicula.addEventListener('mouseenter', (e) => {
+pics.forEach((pic) => {
+	pic.addEventListener('mouseenter', (e) => {
 		const elemento = e.currentTarget;
 		setTimeout(() => {
-			peliculas.forEach(pelicula => pelicula.classList.remove('hover'));
+			pics.forEach(pic => pic.classList.remove('hover'));
 			elemento.classList.add('hover');
 		}, 300);
 	});
 });
 
 fila.addEventListener('mouseleave', () => {
-	peliculas.forEach(pelicula => pelicula.classList.remove('hover'));
+	pics.forEach(pic => pic.classList.remove('hover'));
 });
-
-
-// ? ----- ----- MARKER ----- -----
-var marker = document.querySelector('#marker');
-var item = document.querySelectorAll('nav a');
-function indicador (e){
-	marker.style.left= e.offsetLeft+"px";
-	marker.style.Width= e.offsetWidth+"px";
-}
-
-item.forEach(link => {
-	link.addEventListener('click', (e)=>{
-		indicador (e.target)
-	})
-})
 
 
