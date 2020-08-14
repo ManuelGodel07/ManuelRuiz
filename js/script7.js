0b4245d3be1ec474d52cf2e4199ad684469b8c38
Manuel_Ruiz <manuel_ruiz@ciencias.unam.mx>
2020-08-14 16:54:45 -0500

    nuevodia3

--- js/script7.js
+++ js/script7.js
@@ -320,7 +320,6 @@ export default function hamburgerMenu(panelBtn,panel, menuLink){
 		if(e.target.matches(panelBtn)||e.target.matches(`${paqnelBtn} *`)){
 			d.querySelector(panel).classList.toggle("is-active")
 			d.querySelector(panelBtn).classList.toggle("is-active")
-
 		}
 		if(e.target.matches(menuLink)){
 			d.querySelector(panel).classList.remove("is-active")
