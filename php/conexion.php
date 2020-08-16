c413c5ca6fddf4d44647c2afc012159cbfe8d392
Manuel_Ruiz <manuel_ruiz@ciencias.unam.mx>
2020-08-16 16:42:48 -0500

    pruebaphp

---
+++ php/conexion.php
@@ -0,0 +1,12 @@
+<?php
+    try {
+        $base = new PDO('mysql:host=localhost; dbname=ods_db', 'root', '');
+        $base->setAttribute(PDO:ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $base->exec("SET CHARACTER SET UTF8");
+    } catch (Exception $e) {
+        alert("Linea del error" . $e->getLine());
+        die('Error' . $e->getMessage());
+    }
+
+
+?>
---
+++ php/index7.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>PHP</title>
+        <link rel="stylesheet" type="text/css" href="#">
+        <link rel="shortcut icon" href="../favicon/man.ico">
+    </head>
+    <body>
+        <h1>Hola Mundo</h1>
+        <?php
+            include("conexion.php")
+        ?>   
+    </body>
+</html>
