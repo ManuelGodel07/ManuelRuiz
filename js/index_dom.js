a9bbaa06aa4d8e3d5a760fdcd345c68fe3d1b6c5
Manuel_Ruiz <manuel_ruiz@ciencias.unam.mx>
2020-08-27 19:06:52 -0500

    karent

---
+++ js/index_dom.js
@@ -0,0 +1,8 @@
+import hamburguerMenu from "./hamburguesa.js";
+
+const d= document;
+d.addEventListener("DOMContentLoaded",(e)=>{
+    hamburguerMenu(".panel-btn",".panel",".menu a");
+});
+
+console.log("hola mundo");
