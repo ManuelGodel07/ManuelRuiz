ae41821bbf9eff12ee71c45d2d11b7413b89226f
Manuel_Ruiz <manuel_ruiz@ciencias.unam.mx>
2020-08-29 11:18:00 -0500

    php1

--- php/conexion.php
+++ php/conexion.php
@@ -1,7 +1,7 @@
 <?php
     try {
         $base = new PDO('mysql:host=localhost; dbname=ods_db', 'root', '');
-        $base->setAttribute(PDO:ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $base->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $base->exec("SET CHARACTER SET UTF8");
     } catch (Exception $e) {
         alert("Linea del error" . $e->getLine());
--- php/index7.php
+++ php/index7.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
-<html lang="es">
+<html lang="es-MES">
     <head>
+        
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>PHP</title>
@@ -9,22 +10,9 @@
     </head>
     <body>
         <h1>Hola Mundo, que cositas :3</h1>
-
-    <?php
-        <!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Document</title>
-        </head>
-        <body>
-            <p>Prueba 2</p>
-           
-        </body>
-        </html>
-        
-    include("conexion.php")
-    ?>   
+        <p>Prueba 2</p>
+        <?php
+            include("conexion.php")
+        ?>   
     </body>
 </html>
