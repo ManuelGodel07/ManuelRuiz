facbb878ec9ace50e9ea26530b23d1b7b11e3253
Manuel_Ruiz <manuel_ruiz@ciencias.unam.mx>
2020-09-22 21:22:42 -0500

    contador

--- js/script13.js
+++ js/script13.js
@@ -1,22 +1,26 @@
-function puesto(){
+/****function puesto(){
     let ocupacion = (document.getElementById("puesto").value);
     document.getElementById("puesto").innerHTML=200;
 }
 puesto();
+**/
+function sumar() {
+    let diasT= parseInt(document.getElementById("dias").value);
+    let suldoD= parseFloat(document.getElementById("sueldo_diario").value);
+    alert(diasT*suldoD)
+
+}
+function limpiar() {
+    document.getElementById('calc').onreset();
+}
+
+
+let nombre= "manuel";
+//document.getElementById("txt").innerHTML= nombre;
+
 
 function resultado() {
     let diasT= parseInt(document.getElementById("dias").value);
     let suldoD= parseFloat(document.getElementById("sueldo_diario").value);
-    document.getElementById("sueldoB").innerHTML= "El total a pagar es: " + (diasT*suldoD);
-}
-
-
-function sumar() {
-    let diasT= parseInt(document.getElementById("dias").value);
-    let suldoD= parseFloat(document.getElementById("sueldo_diario").value);
-    alert(diasT+suldoD)
-    
-}
-function limpiar() {
-    document.getElementById('calc').onreset();
+    document.getElementById("sueldoB").innerHTML= "El total a pagar es: " + `(${diasT}*${suldoD})`;
 }
