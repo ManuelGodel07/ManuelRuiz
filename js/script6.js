5d96b6531cce25e9aa43769b7a41068e914c9eee
Manuel_Ruiz <manuel_ruiz@ciencias.unam.mx>
2020-10-11 22:08:44 -0500

    terminando

--- js/script6.js
+++ js/script6.js
@@ -1,10 +1,9 @@
-const fila = document.querySelector('.contenedor-carousel');
-const pics = document.querySelectorAll('.pic');
+const fila = document.querySelector('.contenedor-carousel'),
+	pics = document.querySelectorAll('.pic'),
+	flechaIzquierda = document.getElementById('flecha-izquierda'),
+	flechaDerecha = document.getElementById('flecha-derecha');
 
-const flechaIzquierda = document.getElementById('flecha-izquierda');
-const flechaDerecha = document.getElementById('flecha-derecha');
-
-// ? ----- ----- Event Listener para la flecha derecha. ----- -----
+// ----- ----- Event Listener para la flecha derecha. ----- -----
 flechaDerecha.addEventListener('click', () => {
 	fila.scrollLeft += fila.offsetWidth;
 
@@ -47,48 +46,92 @@ fila.addEventListener('mouseleave', () => {
 
 
 
-const fila = document.querySelector('.contenedor-carousel-2');
-const pics = document.querySelectorAll('.pic');
-
-const flechaIzquierda = document.getElementById('flecha-izquierda');
-const flechaDerecha = document.getElementById('flecha-derecha');
+const fila2 = document.querySelector('.contenedor-carousel2'),
+	pics2 = document.querySelectorAll('.pic2'),
+	flechaIzquierda2 = document.getElementById('flecha-izquierda2'),
+	flechaDerecha2 = document.getElementById('flecha-derecha2');
 
 // ? ----- ----- Event Listener para la flecha derecha. ----- -----
-flechaDerecha.addEventListener('click', () => {
-	fila.scrollLeft += fila.offsetWidth;
+flechaDerecha2.addEventListener('click', () => {
+	fila2.scrollLeft += fila2.offsetWidth;
 
-	const indicadorActivo = document.querySelector('.indicadores .activo');
-	if(indicadorActivo.nextSibling){
-		indicadorActivo.nextSibling.classList.add('activo');
-		indicadorActivo.classList.remove('activo');
+	const indicadorActivo2 = document.querySelector('.indicadores .activo');
+	if(indicadorActivo2.nextSibling){
+		indicadorActivo2.nextSibling.classList.add('activo');
+		indicadorActivo2.classList.remove('activo');
 	}
 });
 
 // ? ----- ----- Event Listener para la flecha izquierda. ----- -----
-flechaIzquierda.addEventListener('click', () => {
-	fila.scrollLeft -= fila.offsetWidth;
+flechaIzquierda2.addEventListener('click', () => {
+	fila2.scrollLeft -= fila2.offsetWidth;
 
-	const indicadorActivo = document.querySelector('.indicadores .activo');
-	if(indicadorActivo.previousSibling){
-		indicadorActivo.previousSibling.classList.add('activo');
-		indicadorActivo.classList.remove('activo');
+	const indicadorActivo2 = document.querySelector('.indicadores .activo');
+	if(indicadorActivo2.previousSibling){
+		indicadorActivo2.previousSibling.classList.add('activo');
+		indicadorActivo2.classList.remove('activo');
 	}
 });
 
 
 
 // ? ----- ----- Hover ----- -----
-pics.forEach((pic) => {
-	pic.addEventListener('mouseenter', (e) => {
+pics.forEach((pic2) => {
+	pic2.addEventListener('mouseenter', (e) => {
 		const elemento = e.currentTarget;
 		setTimeout(() => {
-			pics.forEach(pic => pic.classList.remove('hover'));
+			pics.forEach(pic2 => pic2.classList.remove('hover'));
 			elemento.classList.add('hover');
 		}, 300);
 	});
 });
 
 fila.addEventListener('mouseleave', () => {
-	pics.forEach(pic => pic.classList.remove('hover'));
+	pics.forEach(pic2 => pic2.classList.remove('hover'));
 });
 
+
+
+const fila3 = document.querySelector('.contenedor-carousel3'),
+	pics3 = document.querySelectorAll('.pic3'),
+	flechaIzquierda3 = document.getElementById('flecha-izquierda3'),
+	flechaDerecha3 = document.getElementById('flecha-derecha3');
+
+// ? ----- ----- Event Listener para la flecha derecha. ----- -----
+flechaDerecha3.addEventListener('click', () => {
+	fila3.scrollLeft += fila3.offsetWidth;
+
+	const indicadorActivo3 = document.querySelector('.indicadores .activo');
+	if(indicadorActivo3.nextSibling){
+		indicadorActivo3.nextSibling.classList.add('activo');
+		indicadorActivo3.classList.remove('activo');
+	}
+});
+
+// ? ----- ----- Event Listener para la flecha izquierda. ----- -----
+flechaIzquierda3.addEventListener('click', () => {
+	fila3.scrollLeft -= fila3.offsetWidth;
+
+	const indicadorActivo3 = document.querySelector('.indicadores .activo');
+	if(indicadorActivo3.previousSibling){
+		indicadorActivo3.previousSibling.classList.add('activo');
+		indicadorActivo3.classList.remove('activo');
+	}
+});
+
+
+
+// ? ----- ----- Hover ----- -----
+pics.forEach((pic3) => {
+	pic3.addEventListener('mouseenter', (e) => {
+		const elemento = e.currentTarget;
+		setTimeout(() => {
+			pics.forEach(pic3 => pic3.classList.remove('hover'));
+			elemento.classList.add('hover');
+		}, 300);
+	});
+});
+
+fila3.addEventListener('mouseleave', () => {
+	pics.forEach(pic3 => pic3.classList.remove('hover'));
+});
